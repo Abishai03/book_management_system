cbbbd3c779a87f80313149600c3a0a0d9ac4fc4e
Abishai03 <abishai.singh@gmail.com>
2023-10-13 22:18:17 -0400

    Seperated database module.

---
+++ app/db_layer.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+db_file = 'app/data/books.json'
+
+def get_books_from_json() -> dict:
+    try:
+        with open(db_file, 'r') as file:
+            books = json.load(file)
+        return books
+    except Exception as e:
+        return [f"{e}"]
+
+def add_book_to_json(book: dict) -> bool:
+    try:
+        with open(db_file, 'r') as file:
+            books_load = json.load(file)
+
+        books_load.append(book)
+
+        with open(db_file, 'w') as file:
+            json.dump(books_load, file)
+
+        return True
+    except Exception as e:
+        return False
+
+def remove_book_from_json(ISDN: str) -> bool:
+    # TODO: Implement this function
+    pass
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,11 @@
-import json
 from app import app
-from flask import render_template, request, redirect, url_for, jsonify
-import os
-
+from flask import render_template, request, redirect, url_for
+from .db_layer import *
 
 
 @app.route('/')
 def index():
-    with open('app/data/books.json', 'r') as file:
-        books = json.load(file)
+    books = get_books_from_json()
     return render_template('index.html', books=books)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,11 +18,6 @@ def login():
             error = 'Incorrect username or password. Please try again.'
     return render_template('login.html', error=error)
 
-
-@app.route('/logged_in')
-def logged_in():
-    return 'Logged In'
-
 @app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
@@ -36,16 +28,12 @@ def add_book():
             "price": float(request.form['price']),
             "description": request.form['description']
         }
-        try:
-            with open('app/data/books.json', 'r') as file:
-                books = json.load(file)
 
-            books.append(new_book)
-
-            with open('app/data/books.json', 'w') as file:
-                json.dump(books, file)
-
+        result = add_book_to_json(new_book)
+        
+        if result:
             return render_template('add_book.html', message="Successfully added to database. Return to home.")
-        except Exception as e:
+        else:
             return render_template('add_book.html', message="Failed to add book to database. Please try again.")
+        
     return render_template('add_book.html', message=None)
