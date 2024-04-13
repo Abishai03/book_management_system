266782d1325e3f24643cf1804cbf1d88fc073929
azizullah2017 <p146111@nu.edu.pk>
2024-04-13 22:33:50 +0500

    made some changes

--- app/chat_db.py
+++ app/chat_db.py
@@ -5,8 +5,8 @@ from langchain_community.utilities import SQLDatabase
 from dotenv import load_dotenv
 load_dotenv()
 
-OpenAI_Key = ""
-# OpenAI_Key =  os.getenv('OpenAI')
+# OpenAI_Key = ""
+OpenAI_Key =  os.getenv('OpenAI')
 db = SQLDatabase.from_uri("sqlite:///instance/database.db", include_tables=["book"])
 llm = OpenAI(temperature=0, openai_api_key=OpenAI_Key)
 db_chain = SQLDatabaseChain.from_llm(llm, db)
